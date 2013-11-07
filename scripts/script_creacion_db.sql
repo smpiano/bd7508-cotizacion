bd0625823eb9122661defae25b74f6d3e3c12731
Sergio Piano <smpiano@gmail.com>
2013-11-07 03:35:05 -0300

    Update sobre el script de creacion de la base de datos 1.- Agregando telefono y email al cliente 2.- Agregando los NOT NULL 3.- Borrando caracteres unicode

--- scripts/script_creacion_db.sql
+++ scripts/script_creacion_db.sql
@@ -1,29 +1,29 @@
 -- GRUPO 16
 -- Cotizador de Polizas
 -- Script de creacion.
-﻿-- Zona
+-- Zona
 CREATE TABLE Zona(
-	nombre varchar(50),
-	nroZona integer,
+	nombre varchar(50) NOT NULL,
+	nroZona integer NOT NULL,
 	PRIMARY KEY (nroZona)
 );
 -- Sucursal
 CREATE TABLE Sucursal(
-	nroSucursal integer,
-	nombre varchar(50),
+	nroSucursal integer NOT NULL,
+	nombre varchar(50) NOT NULL,
 	PRIMARY KEY (nroSucursal)
 );
 -- Rubro
 CREATE TABLE Rubro(
-	nroRubro integer,
-	nombre varchar(50),
+	nroRubro integer NOT NULL,
+	nombre varchar(50) NOT NULL,
 	descripcion varchar(500),
 	PRIMARY KEY (nroRubro)
 );
 -- Cobertura
 CREATE TABLE Cobertura(
-	nroCobertura integer,
-	nombre varchar(50),
+	nroCobertura integer NOT NULL,
+	nombre varchar(50) NOT NULL,
 	reaseguro varchar(50),
 	coseguro varchar(50),
 	descripcion varchar(50),
@@ -31,62 +31,64 @@ CREATE TABLE Cobertura(
 );
 -- Cliente
 CREATE TABLE Cliente(
-	dni integer,
-	apellido varchar(100),
-	nombre varchar(100),
-	domicilio varchar(200),
+	dni integer NOT NULL,
+	apellido varchar(100) NOT NULL,
+	nombre varchar(100) NOT NULL,
+	domicilio varchar(200) NOT NULL,
+	telefono varchar(100) NOT NULL,
+	email varchar(100) NOT NULL,
 	PRIMARY KEY (dni)
 );
 -- Empleado
 CREATE TABLE Empleado(
-	legajo integer,
-	dni integer,
-	apellido varchar(100),
-	nombre varchar(100),
-	nroSucursal integer,
+	legajo integer NOT NULL,
+	dni integer NOT NULL,
+	apellido varchar(100) NOT NULL,
+	nombre varchar(100) NOT NULL,
+	nroSucursal integer NOT NULL,
 	PRIMARY KEY (legajo),
 	CONSTRAINT fk_empl_sucursal FOREIGN KEY (nroSucursal) REFERENCES Sucursal(nroSucursal)
 );
 -- Mesa Entrada
 CREATE TABLE MesaEntrada(
-	legajo integer,
+	legajo integer NOT NULL,
 	PRIMARY KEY (legajo),
 	CONSTRAINT fk_mesaEnt_empl FOREIGN KEY (legajo) REFERENCES Empleado(legajo)
 );
 -- Inspector
 CREATE TABLE Inspector(
-	legajo integer,
+	legajo integer NOT NULL,
 	PRIMARY KEY (legajo),
 	CONSTRAINT fk_inspec_empl FOREIGN KEY (legajo) REFERENCES Empleado(legajo)
 );
 -- EjecutivoCuentas
 CREATE TABLE EjecutivoCuentas(
-	legajo integer,
-	nroZona integer,
+	legajo integer NOT NULL,
+	nroZona integer NOT NULL,
 	PRIMARY KEY (legajo),
 	CONSTRAINT fk_ejecCue_zona FOREIGN KEY (nroZona) REFERENCES Zona(nroZona),
 	CONSTRAINT fk_ejecCue_empl FOREIGN KEY (legajo) REFERENCES Empleado(legajo)
 );
 -- Cubre
 CREATE TABLE Cubre(
-	nroRubro integer,
-	nroCobertura integer,
+	nroRubro integer NOT NULL,
+	nroCobertura integer NOT NULL,
 	PRIMARY KEY	(nroRubro ,nroCobertura),
 	CONSTRAINT fk_cubre_rubro FOREIGN KEY (nroRubro) REFERENCES Rubro(nroRubro),
 	CONSTRAINT fk_cubre_cober FOREIGN KEY (nroCobertura) REFERENCES Cobertura(nroCobertura)
 );
 -- Cotizacion
 CREATE TABLE Cotizacion(
-	nroCotizacion integer,
-	tipoVehiculo varchar(50),
-	cantCuota integer,
-	fechaCotizacion date,
-	fechaExpiracion date,
-	dni integer,
-	nroRubro integer,
-	legajo integer,
-	nroSucursal integer,
-	aprobacion varchar(50),
+	nroCotizacion integer NOT NULL,
+	tipoVehiculo varchar(50) NOT NULL,
+	cantCuota integer NOT NULL,
+	fechaCotizacion date NOT NULL,
+	fechaExpiracion date NOT NULL,
+	dni integer NOT NULL,
+	nroRubro integer NOT NULL,
+	legajo integer NOT NULL,
+	nroSucursal integer NOT NULL,
+	aprobacion varchar(50) NOT NULL,
 	PRIMARY KEY (nroCotizacion),
 	CONSTRAINT fk_cotiz_clie FOREIGN KEY (dni) REFERENCES Cliente(dni),
 	CONSTRAINT fk_cotiz_rubro FOREIGN KEY (nroRubro) REFERENCES Rubro(nroRubro),
@@ -95,85 +97,69 @@ CREATE TABLE Cotizacion(
 );
 -- Autoriza Cotizacion
 CREATE TABLE AutorizaCotizacion(
-	nroCotizacion integer,
-	legajo integer,
+	nroCotizacion integer NOT NULL,
+	legajo integer NOT NULL,
 	PRIMARY KEY (nroCotizacion),
 	CONSTRAINT fk_autorCot_cotiz FOREIGN KEY (nroCotizacion) REFERENCES Cotizacion(nroCotizacion),
 	CONSTRAINT fk_autorCot_empl FOREIGN KEY (legajo) REFERENCES Empleado(legajo)
 );
 -- Analiza Cotizacion
 CREATE TABLE AnalizaCotizacion(
-	nroCotizacion integer,
-	legajo integer,
+	nroCotizacion integer NOT NULL,
+	legajo integer NOT NULL,
 	PRIMARY KEY (nroCotizacion),
 	CONSTRAINT fk_analCot_cotiz FOREIGN KEY (nroCotizacion) REFERENCES Cotizacion(nroCotizacion),
 	CONSTRAINT fk_analCot_empl FOREIGN KEY (legajo) REFERENCES Empleado(legajo)
 );
--- Relacion Trabaja
-CREATE TABLE RelacionTrabaja(
-	legajo integer,
-	nroSucursal integer,
-	PRIMARY KEY (legajo),
-	CONSTRAINT fk_rtrabaja_empl FOREIGN KEY (legajo) REFERENCES Empleado(legajo),
-	CONSTRAINT fk_rtrabaja_sucu FOREIGN KEY (nroSucursal) REFERENCES Sucursal(nroSucursal)
-);
--- Relacion Solicita Cotizacion
-CREATE TABLE RelacionSolicitaCotizacion(
-	nroCotizacion integer,
-	dni integer,
-	PRIMARY KEY (nroCotizacion),
-	CONSTRAINT fk_rsolic_cotiz FOREIGN KEY (nroCotizacion) REFERENCES Cotizacion(nroCotizacion),
-	CONSTRAINT fk_rsolic_clie FOREIGN KEY (dni) REFERENCES Cliente(dni)
-);
 -- Productor
 CREATE TABLE Productor(
-	legajo integer,
+	legajo integer NOT NULL,
 	PRIMARY KEY (legajo),
 	CONSTRAINT fk_productor_empl FOREIGN KEY (legajo) REFERENCES Empleado(legajo)
 );
 -- Posee Cobertura
 CREATE TABLE PoseeCobertura(
-	nroCotizacion integer,
-	nroCobertura integer,
+	nroCotizacion integer NOT NULL,
+	nroCobertura integer NOT NULL,
 	PRIMARY KEY (nroCotizacion, nroCobertura),
 	CONSTRAINT fk_rposCob_cotiz FOREIGN KEY (nroCotizacion) REFERENCES Cotizacion(nroCotizacion),
 	CONSTRAINT fk_rposCob_cober FOREIGN KEY (nroCobertura) REFERENCES Cobertura(nroCobertura)
 );
 -- Poliza
 CREATE TABLE Poliza(
-	nroPoliza integer,
-	nroCotizacion integer,
-	dni integer,
-	fechaEmision date,
-	fechaVigencia date,
+	nroPoliza integer NOT NULL,
+	nroCotizacion integer NOT NULL,
+	dni integer NOT NULL,
+	fechaEmision date NOT NULL,
+	fechaVigencia date NOT NULL,
 	PRIMARY KEY (nroPoliza),
 	CONSTRAINT fk_poliz_cotiz FOREIGN KEY (nroCotizacion) REFERENCES Cotizacion(nroCotizacion),
 	CONSTRAINT fk_poliz_clie FOREIGN KEY (dni) REFERENCES Cliente(dni)
 );
 -- Cuotas
 CREATE TABLE Cuotas(
-	nroCuota integer,
-	nroCotizacion integer,
-	fechaVencimiento date,
-	monto integer,
+	nroCuota integer NOT NULL,
+	nroCotizacion integer NOT NULL,
+	fechaVencimiento date NOT NULL,
+	monto integer NOT NULL,
 	PRIMARY KEY (nroCuota, nroCotizacion),
 	CONSTRAINT fk_cuot_cotiz FOREIGN KEY (nroCotizacion) REFERENCES Cotizacion(nroCotizacion)
 );
 -- Inspeccion
 CREATE TABLE Inspeccion(
-	nroInspeccion integer,
-	aprobado varchar(50),
+	nroInspeccion integer NOT NULL,
+	aprobado varchar(50) NOT NULL,
 	descripcion varchar(500),
-	legajo integer,
-	fechaInspeccion date,
-	nroCotizacion integer,
+	legajo integer NOT NULL,
+	fechaInspeccion date NOT NULL,
+	nroCotizacion integer NOT NULL,
 	PRIMARY KEY (nroInspeccion, nroCotizacion),
 	CONSTRAINT fk_inspe_cotiz FOREIGN KEY (nroCotizacion) REFERENCES Cotizacion(nroCotizacion),
 	CONSTRAINT fk_inspe_empl FOREIGN KEY (legajo) REFERENCES Empleado(legajo)
 );
 -- Gerencia
 CREATE TABLE Gerencia(
-	legajo integer,
+	legajo integer NOT NULL,
 	PRIMARY KEY (legajo),
 	CONSTRAINT fk_gerenc_empl FOREIGN KEY (legajo) REFERENCES Empleado(legajo)
 );
